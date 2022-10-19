c0a1a3fddc1dd13ff36c05f4bf64598d33c74769
swapnil <swapnil.kaspale1@gmail.com>
2022-10-19 12:52:30 -0400

    Fixed bug user name not displayed

--- src/components/Users/UserList.js
+++ src/components/Users/UserList.js
@@ -8,7 +8,7 @@ const UserList = (props) => {
     <ul>
       {props.users.map(user => (
       <li key={user.id}>
-        {user.name} ({user.age} years old)
+        {user.username} ({user.age} years old)
       </li>
       ))}
     </ul>
